483d5e06e3bbc8c65cdae56c9a173975f05b4e1e
Conor Heine <conor.heine@gmail.com>
2024-01-05 12:00:22 -0800

    update tests

--- tests/test_cli/test_parse.py
+++ tests/test_cli/test_parse.py
@@ -1,4 +1,5 @@
 import pytest
+import yaml
 
 
 @pytest.mark.parametrize(
@@ -13,5 +14,57 @@ import pytest
 def test_cli_parse(cmd_args, cli_invoke, tmp_path, example_resume_yaml_contents):
     resume_file = tmp_path / "test-resume.yml"
     resume_file.write_text(example_resume_yaml_contents)
-    result = cli_invoke(["parse", str(resume_file), *cmd_args])
+    output_file = tmp_path / "test-resume.md"
+    assert output_file.exists() is False
+    result = cli_invoke(["parse", str(resume_file), str(output_file), *cmd_args])
     assert result.exit_code == 0
+    assert output_file.exists()
+    assert output_file.stat().st_size > 0
+
+
+@pytest.mark.parametrize(
+    "cmd_options",
+    [
+        ["-c"],
+        ["--check"],
+    ],
+)
+def test_cli_parse_check_only(cmd_options, cli_invoke, tmp_path, example_resume_yaml_contents):
+    resume_file = tmp_path / "test-resume.yml"
+    resume_file.write_text(example_resume_yaml_contents)
+    output_file = tmp_path / "test-resume.md"
+    assert output_file.exists() is False
+    result = cli_invoke(["parse", str(resume_file), *cmd_options])
+    assert result.exit_code == 0
+    assert not output_file.exists()
+
+
+@pytest.mark.parametrize(
+    "invalid_section_contents",
+    [
+        pytest.param("invalid", id="invalid"),
+        pytest.param(None, id="null"),
+        pytest.param(1, id="int"),
+        pytest.param(1.0, id="float"),
+        pytest.param(True, id="bool"),
+    ],
+)
+def test_cli_parse_errors_on_invalid_section_contents(
+    invalid_section_contents, cli_invoke, tmp_path, example_resume_yaml_contents
+):
+    resume_data = yaml.safe_load(example_resume_yaml_contents)
+    resume_data["resume"]["sections"][0]["contents"] = invalid_section_contents
+    resume_file = tmp_path / "test-resume.yml"
+    resume_file.write_text(yaml.dump(resume_data))
+    output_file = tmp_path / "test-resume.md"
+    assert output_file.exists() is False
+    result = cli_invoke(["parse", str(resume_file), str(output_file)])
+    assert result.exit_code != 0
+    assert not output_file.exists()
+
+
+def test_error_loading_yaml(cli_invoke, tmp_path):
+    resume_file = tmp_path / "test-invalid-content.yml"
+    resume_file.write_text("invalid")
+    result = cli_invoke(["parse", "-c", str(resume_file)])
+    assert result.exit_code != 0
--- tests/test_package_defaults.py
+++ tests/test_package_defaults.py
@@ -1,5 +1,4 @@
-from jobapppy import __version__
-
-
 def test_version():
+    from jobapppy import __version__
+
     assert __version__ == "0.0.1"
