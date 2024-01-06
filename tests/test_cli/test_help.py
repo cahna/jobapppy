9a9bcabab47eca6d7e4b955dca8bc86fdd6ec6e8
Conor Heine <conor.heine@gmail.com>
2024-01-06 13:11:44 -0800

    relax test to account for colored outputs

--- tests/test_cli/test_help.py
+++ tests/test_cli/test_help.py
@@ -1,4 +1,4 @@
 def test_help(cli_invoke):
     result = cli_invoke(["--help"])
     assert result.exit_code == 0
-    assert result.stdout.lstrip().startswith("Usage:")
+    assert "Usage:" in result.stdout.strip()
